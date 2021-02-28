c38cf402e1406e59c5273498c8db6b0979661cc1
micyakobi <64555783+micyakobi@users.noreply.github.com>
2021-02-28 03:16:00 +0200

    Added Register-page, firebase auth and more

--- app/src/main/java/com/example/foody/MainActivity.java
+++ app/src/main/java/com/example/foody/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         backgroundImageView = findViewById(R.id.main_background_image_view);
         exploreBtn = findViewById(R.id.main_explore_button);
-        animateBackground();
+        Utils.animateBackground(backgroundImageView);
 
         exploreBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,10 +31,6 @@ public class MainActivity extends AppCompatActivity {
                 toLoginPage();
             }
         });
-    }
-
-    private void animateBackground(){
-        backgroundImageView.animate().scaleX((float) 1.5).scaleY((float) 1.5).setDuration(20000).start();
     }
 
     private void toLoginPage(){
--- app/src/main/java/com/example/foody/activities/LoginPageActivity.java
+++ app/src/main/java/com/example/foody/activities/LoginPageActivity.java
@@ -1,11 +1,16 @@
 package com.example.foody.activities;
 import com.example.foody.R;
+import com.example.foody.Utils;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import android.util.Log;
+import android.view.View;
+import android.content.Intent;
 
 public class LoginPageActivity extends AppCompatActivity {
 
@@ -26,13 +31,28 @@ public class LoginPageActivity extends AppCompatActivity {
         emailInput = findViewById(R.id.login_activity_email_edit_text);
         passwordInput = findViewById(R.id.login_activity_password_edit_text);
         loginBtn = findViewById(R.id.login_activity_login_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("TAG", "Attempt to login");
+            }
+        });
+
         registerBtn = findViewById(R.id.login_activity_register_btn);
-
-        animateBackground();
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toRegisterPage();
+            }
+        });
+        Utils.animateBackground(backgroundImageView);
 
     }
 
-    private void animateBackground(){
-        backgroundImageView.animate().scaleX((float) 1.5).scaleY((float) 1.5).setDuration(20000).start();
+    private void toRegisterPage(){
+        Intent intent = new Intent(this, RegisterPageActivity.class);
+        startActivity(intent);
     }
+
 }
