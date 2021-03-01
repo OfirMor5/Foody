02f095a0713cdec91f1c827bf6a4350e1830a3ed
micyakobi <64555783+micyakobi@users.noreply.github.com>
2021-03-01 17:01:19 +0200

    Fixed bugs

--- app/src/main/java/com/example/foody/activities/HomeActivity.java
+++ app/src/main/java/com/example/foody/activities/HomeActivity.java
@@ -1,16 +1,19 @@
 package com.example.foody.activities;
 import com.example.foody.R;
 import com.example.foody.fragments.FeedListFrag;
+import com.example.foody.fragments.FeedListFragDirections;
 import com.example.foody.model.Post;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
 import android.os.Bundle;
+import androidx.navigation.Navigation;
+import androidx.navigation.NavController;
+import androidx.navigation.ui.NavigationUI;
+import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class HomeActivity extends AppCompatActivity  {
+
+
+public class HomeActivity extends AppCompatActivity implements FeedListFrag.Delegate  {
 
     NavController navC;
 
@@ -24,10 +27,10 @@ public class HomeActivity extends AppCompatActivity  {
         NavigationUI.setupWithNavController(bottomNav, navC);
     }
 
-//    @Override
-//    public void onItemSelected(Post post) {
-//        navCtrl = Navigation.findNavController(this, R.id.home_nav_host);
-//        FeedListFragmentDirections.ActionFeedListFragmentToPostDetailsFragment directions = FeedListFragmentDirections.actionFeedListFragmentToPostDetailsFragment(post);
-//        navCtrl.navigate(directions);
-//    }
+    @Override
+    public void onItemSelected(Post post) {
+        navC = Navigation.findNavController(this, R.id.home_nav_host);
+        FeedListFragDirections.ActionFeedListFragmentToPostDetailsFragment directions = FeedListFragDirections.actionFeedListFragmentToPostDetailsFragment(post);
+        navC.navigate(directions);
+    }
 }
--- app/src/main/java/com/example/foody/model/AppLocalDB.java
+++ app/src/main/java/com/example/foody/model/AppLocalDB.java
@@ -5,7 +5,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Post.class}, version = 1)
+@Database(entities = {Post.class}, version = 7)
 abstract class AppLocalDbRepository extends RoomDatabase {
     public abstract PostDao postDao();
 }
@@ -16,7 +16,7 @@ public class AppLocalDB {
             Room.databaseBuilder(
                     FoodyApp.context,
                     AppLocalDbRepository.class,
-                    "localDb.db")
+                    "AppLocalDb.db")
                     .fallbackToDestructiveMigration()
                     .build();
 
